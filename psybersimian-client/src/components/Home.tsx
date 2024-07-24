cae82b08cfb288d7421b8db6b76db8fe7387ee60
Triston Burns <tburns@aurasolutionsllc.com>
2024-07-24 00:44:01 -0700

    Fix card height issue on iPhone SE screens

--- psybersimian-client/src/components/Home.tsx
+++ psybersimian-client/src/components/Home.tsx
@@ -45,27 +45,27 @@ export const Home = () => {
   return (
     <>
       <div id="home">
-        <p className="press-start-2p-regular text-nowrap text-sm text-ivory text-center z-20 mt-44 absolute ms-5 md:text-2xl lg:text-4xl md:text-wrap md:ms-9">{tagLine}</p>
+        <p className="press-start-2p-regular text-nowrap text-super-xs ms-2 text-ivory text-center z-20 mt-44 absolute sm:ms-5 sm:text-sm md:text-2xl lg:text-4xl md:text-wrap md:ms-9">{tagLine}</p>
         { count === baseTagLine.length ? <p className="ibm-plex-sans-regular text-balance text-xl text-ivory text-center z-20 mt-72 absolute ms-3 animate-fade md:text-5xl lg:text-7xl md:text-wrap">Crafting tailored software solutions to propel business growth and efficiency.</p> : null }
         <Hero isLoaded={isLoaded} setIsLoaded={setIsLoaded} />
         <Section style={"bg-ivory h-screen"}>
-          <Card animationStyle={"card box-border h-5/6 w-3/4 p-4 shadow-lg m-10 shadow-black"}>
+          <Card style={"card box-border m-5 w-3/4 p-4 shadow-lg shadow-black lg:m-10"}>
             <h2 className="ibm-plex-sans-regular text-2xl text-midnight-black text-center mt-2 lg:text-5xl">Applications</h2>
-            <div className="flex flex-col text-start ms-10 p-5 justify-around">
+            <div className="flex flex-col text-start ms-5 lg:ms-10 p-5 justify-around">
               <div className='flex flex-row'>
                 <Building2 className='me-10 lg:mt-2' />
                 <h3 className='text-sm lg:text-3xl'>Small Business</h3>
               </div>
               <Paragraph textStyle={"ibm-plex-sans-regular text-xs text-midnight-black text-start mt-2 lg:text-xl"} text={'Our products fulfill diverse needs of small businesses by delivering professional, high-quality, and dependable systems. Our team specializes in developing e-commerce, informational, and service-based applications, so small businesses quickly establish an online presence.'}/>
             </div>
-            <div className="flex flex-col text-start ms-10 p-5 justify-around">
+            <div className="flex flex-col text-start ms-5 lg:ms-10 p-5 justify-around">
               <div className='flex flex-row'>
                 <BriefcaseBusiness  className='me-10 lg:mt-2' />
                 <h3 className='text-sm lg:text-3xl'>Enterprise</h3>
               </div>
               <Paragraph textStyle={"ibm-plex-sans-regular text-xs text-midnight-black text-start mt-2 lg:text-xl"} text={'We have experience with developing maintainable, long-lasting enterprise level solutions in the corporate community management and HOA industries.'}/>
             </div>
-            <div className="flex flex-col text-start ms-10 p-5 justify-around">
+            <div className="flex flex-col text-start ms-5 lg:ms-10 p-5 justify-around">
               <div className='flex flex-row'>
                 <Aperture className='me-10 lg:mt-2' />
                 <h3 className='text-sm lg:text-3xl'>Government</h3>
@@ -75,21 +75,21 @@ export const Home = () => {
           </Card>
         </Section>
         <Section style={"bg-lamplight-yellow h-screen"}>
-          <Card animationStyle={"card box-border h-6/12 w-3/4 p-4 shadow-lg m-10 shadow-black lg:"}>
+          <Card style={"card box-border w-3/4 p-4 shadow-lg m-10 shadow-black md:h-3/4"}>
             <h2 className="ibm-plex-sans-regular text-5xl text-midnight-black text-center mt-2">Our Team</h2>
             <div className="flex flex-col lg:flex-row justify-center items-center">
-              <img className='h-1/3 w-1/3 box-border shadow-md shadow-black rounded-xl mt-10 lg:mt-3' src={developer} />
-              <Paragraph textStyle={"ibm-plex-sans-regular text-md text-midnight-black text-start mt-5 ms-10 p-5 lg:text-3xl"} text={'Our team comprises a number of talented, well-rounded engineers with solid reputations across multiple public and private sector organizations. We work diligently with our customers so that satisfaction is guaranteed upon product delivery.'}/>
+              <img className='h-2/3 w-2/3 box-border shadow-md shadow-black rounded-xl mt-10 lg:mt-3 lg:h-1/3 lg:w-1/3' src={developer} alt="developer" />
+              <Paragraph textStyle={"ibm-plex-sans-regular text-xs sm:text-md text-midnight-black text-start mt-5 lg:ms-10 p-5 lg:text-3xl"} text={'Our team comprises a number of talented, well-rounded engineers with solid reputations across multiple public and private sector organizations. We work diligently with our customers so that satisfaction is guaranteed upon product delivery.'}/>
             </div>
           </Card>
         </Section>
         <Section style={"bg-honeysuckle h-screen"}>
-          <Card animationStyle={"card box-border h-6/12 w-3/4 p-4 shadow-lg m-10 shadow-black items-center"}>
+          <Card style={"card box-border w-3/4 p-4 shadow-lg m-10 shadow-black items-center"}>
             <h2 className="ibm-plex-sans-regular text-5xl text-midnight-black text-center mt-10">Contact</h2>
             <div className="flex flex-col justify-center items-center">
-              <Paragraph textStyle={"ibm-plex-sans-regular text-md text-midnight-black text-start ms-10 p-10 lg:text-3xl"} text={'Interested in working with our team? Send us an email or give us a call!'}/>
-              <a href="mailto:info@psybersimian.com"><p className='p-5 lg:text-3xl'>Email</p></a>
-              <a href="tel:+5204053987"><p className='lg:text-3xl'>Phone</p></a>
+              <Paragraph textStyle={"ibm-plex-sans-regular text-md text-midnight-black text-start p-10 lg:text-3xl lg:ms-10"} text={'Interested in working with our team? Send us an email or give us a call!'}/>
+              <a href="mailto:info@psybersimian.com"><button className='border rounded-full p-2 m-3 lg:text-3xl'>Email</button></a>
+              <a href="tel:+5204053987"><p className='border rounded-full p-2 m-3 lg:text-3xl'>Phone</p></a>
             </div>
           </Card>
         </Section>
